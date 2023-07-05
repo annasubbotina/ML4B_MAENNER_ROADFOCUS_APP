8deed42cef58482faf94ce42a4bd3c905ce10402
Anna Subbotina <anna.subbotina@fau.de>
2023-07-05 22:18:00 +0500

    code upd

--- CODE_MODEL_APP.py
+++ CODE_MODEL_APP.py
@@ -6,9 +6,31 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
 
+def load_data():
+    bus_1 = pd.read_json("Bus1_Bauvereinstr.-Technische Hochschule-Dürrenhof.23-05-23_18-29-17.json")
+    bus_2 = pd.read_json("Bus2_Dürrenhof-Stephanstr.23-05-23_18-31-17.json")
+    # Add other datasets as needed
+    bus_df = pd.concat([bus_1, bus_2], ignore_index=True)
+    return bus_df
 
 def preprocess_data(df):
-    # Добавьте здесь код предварительной обработки данных, если необходимо
+    # Preprocess data here (remove null values, drop unnecessary columns, calculate speed, etc.)
+    df = df.dropna(subset=['time', 'seconds_elapsed'])
+    df['z'].fillna(df['z'].mean(), inplace=True)
+    df['y'].fillna(df['y'].mean(), inplace=True)
+    df['x'].fillna(df['x'].mean(), inplace=True)
+
+    initial_velocity = 0.0
+    df['seconds_elapsed'] = pd.to_numeric(df['seconds_elapsed'], errors='coerce')
+    df['z'] = pd.to_numeric(df['z'], errors='coerce')
+    df['calculated_speed'] = initial_velocity + df['seconds_elapsed'].diff() * df['z']
+    df.loc[:, 'calculated_speed'].fillna(df['calculated_speed'].mean(), inplace=True)
+
+
+    threshold_acceleration = 0.1
+    df['moving'] = df[['x', 'y', 'z']].apply(lambda row: any(abs(row) > threshold_acceleration), axis=1)
+    df['moving_label'] = df['moving'].astype(int)
+
     return df
 
 
@@ -47,56 +69,52 @@ def main():
     st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
     st.title("MoveMate")
     st.header("Halte alle auf dem Laufenden")
-    
+
     uploaded_file = st.file_uploader("Datei hochladen", type="json")
-    
+
     if uploaded_file is not None:
-        df = pd.read_json(uploaded_file)
-        df = preprocess_data(df)
-        model = train_model(df)
+        user_df = pd.read_json(uploaded_file)
+        user_df = preprocess_data(user_df)  # Исправлено: заменено df на user_df
+        model = train_model(user_df)
+        transportation = predict_transportation(model, user_df)
         
-        movement_prediction = predict_movement(model, df)
-        transportation = predict_transportation(model, df)
-    
-        st.write(f"Aktivität: {bool(movement_prediction[0])}")
         st.write(f"Tranport type: {transportation}")
     else:
         st.write("Laden Sie bitte Datei.json hoch!")
-
 
 if __name__ == "__main__":
     main()
 
 
 
-"""def main():
-    st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
+#def main():
+    #st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
 
-    st.title("MoveMate")
-    st.header("Halte alle auf dem Laufenden")
-    st.write("Um die Funktionen von MoveMate zu nutzen, schauen Sie bitte die Instruktion im linken Menü an!")
+    #st.title("MoveMate")
+    ##st.header("Halte alle auf dem Laufenden")
+    #st.write("Um die Funktionen von MoveMate zu nutzen, schauen Sie bitte die Instruktion im linken Menü an!")
     
-    st.write("Nach dem Lesen der Instruktion und der Konfiguration von SensorLoggerApp und MoveMateBot in Telegram können wir jetzt loslegen!")
+    #st.write("Nach dem Lesen der Instruktion und der Konfiguration von SensorLoggerApp und MoveMateBot in Telegram können wir jetzt loslegen!")
     
-    uploaded_file = st.file_uploader("Datei hochladen", type="json")
-    if uploaded_file is not None:
-        df = pd.read_json(uploaded_file)
-        df = preprocess_data(df)
-        model = train_model(df)
+    #uploaded_file = st.file_uploader("Datei hochladen", type="json")
+    #if uploaded_file is not None:
+        #df = pd.read_json(uploaded_file)
+        #df = preprocess_data(df)
+        #model = train_model(df)
 
-        st.subheader("Пример данных:")
-        st.write(df.head())
+        #st.subheader("Пример данных:")
+        #st.write(df.head())
 
-        st.subheader("Предсказание способа передвижения:")
-        x = st.number_input("Введите значение x", value=0.0)
-        y = st.number_input("Введите значение y", value=0.0)
-        z = st.number_input("Введите значение z", value=0.0)
-        speed = st.number_input("Введите значение скорости", value=0.0)
+        #st.subheader("Предсказание способа передвижения:")
+        #x = st.number_input("Введите значение x", value=0.0)
+        #y = st.number_input("Введите значение y", value=0.0)
+        #z = st.number_input("Введите значение z", value=0.0)
+        #speed = st.number_input("Введите значение скорости", value=0.0)
 
-        new_data = pd.DataFrame({'x': [x], 'y': [y], 'z': [z], 'calculated_speed': [speed]})
-        prediction = predict_transportation(model, new_data)
-        st.write("Результат предсказания:", prediction)
+        #new_data = pd.DataFrame({'x': [x], 'y': [y], 'z': [z], 'calculated_speed': [speed]})
+        #prediction = predict_transportation(model, new_data)
+        #st.write("Результат предсказания:", prediction)
 
 
-if __name__ == "__main__":
-    main()"""
+#if __name__ == "__main__":
+    #main()
