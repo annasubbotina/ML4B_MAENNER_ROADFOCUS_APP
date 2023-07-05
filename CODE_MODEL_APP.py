2afeb0c82267a95f140d70ece2d1bed55f04b340
Anna Subbotina <anna.subbotina@fau.de>
2023-07-05 21:52:03 +0500

    code upd

--- CODE_MODEL_APP.py
+++ CODE_MODEL_APP.py
@@ -1,96 +1,38 @@
-#importing necessery libraries for future analysis of the dataset
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as plt
 import seaborn as sns
-#!pip install tensorflow shap
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
 
-#using pandas library and 'read_json' function to read file as file 
-bus_1= pd.read_json("Bus1_Bauvereinstr.-Technische Hochschule-Dürrenhof.23-05-23_18-29-17.json")
-bus_2= pd.read_json("Bus2_Dürrenhof-Stephanstr.23-05-23_18-31-17.json")
-"""bus_3= pd.read_json("bus-_Gleitwitzertr._-_Langwasser_Mitte-2023-05-24_12-06-59 2.json")
-bus_4= pd.read_json("Bus_Wöhrd-Bauvereinstr.23-05-23_18-28-17.json")
-bus_5= pd.read_json("Bus_Rathenauplatz-Harmoniestr.23-05-23_18-25-46.json")
-bus_6= pd.read_json("Bus_Laufer Tor-Rathenauplatz.23-05-23_18-24-45.json")
-bus_7= pd.read_json("Bus_Harmoniestr.-Wöhrd.23-05-23_18-27-14.json")"""
 
-bus_df = [bus_1, bus_2]
-bus_df  = pd.concat(bus_df,ignore_index=True)
-
-# Проверка наличия пропущенных значений в DataFrame
-print(bus_df.isnull().sum())
-
-bus_df = bus_df.dropna(subset=['time', 'seconds_elapsed'])
-bus_df['z'].fillna(bus_df['z'].mean(), inplace=True)
-bus_df['y'].fillna(bus_df['y'].mean(), inplace=True)
-bus_df['x'].fillna(bus_df['x'].mean(), inplace=True)
-
-#Drop collumns
-bus_df = bus_df.drop(['version','device name','recording time','platform','appVersion','device id', 'sensors','sampleRateMs','qz','qy','qx','qw','roll','pitch','yaw','relativeAltitude','pressure', 'altitudeAboveMeanSeaLevel'], axis=1)
-bus_df = bus_df.drop(['bearingAccuracy', 'speedAccuracy', 'verticalAccuracy', 'horizontalAccuracy', 'speed', 'bearing', 'altitude', 'longitude', 'latitude'], axis=1)
-
-print(bus_df.isnull().sum())
-
-# Начальное значение скорости
-initial_velocity = 0.0
-
-# Преобразование столбцов в числовой формат
-bus_df['seconds_elapsed'] = pd.to_numeric(bus_df['seconds_elapsed'], errors='coerce')
-bus_df['z'] = pd.to_numeric(bus_df['z'], errors='coerce')
-
-# Рассчитываем скорость на основе ускорения с помощью метода численного интегрирования (например, метод трапеций)
-bus_df['calculated_speed'] = initial_velocity + bus_df['seconds_elapsed'].diff() * bus_df['z']
-bus_df.loc[:, 'calculated_speed'].fillna(bus_df['calculated_speed'].mean(), inplace=True)
+def preprocess_data(df):
+    # Добавьте здесь код предварительной обработки данных, если необходимо
+    return df
 
 
-# В результате получим столбец 'calculated_speed', который содержит скорость транспортного средства на основе ускорения
+def train_model(df):
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = df[features]
+    y = df['moving_label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+    return clf
 
-print(bus_df['calculated_speed'])
 
-print(bus_df.isnull().sum())
+def predict_movement(model, df):
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = df[features]
+    prediction = model.predict(X)
+    return prediction
 
-# Пороговое значение для определения движения
-threshold_acceleration = 0.1
-
-# Создаем новый столбец, где будем хранить информацию о движении (True - движение, False - остановка)
-bus_df['moving'] = bus_df[['x', 'y', 'z']].apply(lambda row: any(abs(row) > threshold_acceleration), axis=1)
-
-# В результате получим столбец 'moving', который содержит True или False в зависимости от состояния движения
-print(bus_df['moving'])
-
-# Создаем новый столбец, где будем хранить информацию о движении (1 - движение, 0 - остановка)
-bus_df['moving_label'] = bus_df['moving'].astype(int)
-
-# Выбираем признаки (features) для обучения модели
-features = ['x', 'y', 'z', 'calculated_speed']
-X = bus_df[features]
-y = bus_df['moving_label']
-
-# Разделяем данные на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Создаем и обучаем модель Decision Tree
-clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
-
-# Предсказываем состояние движения на тестовой выборке
-y_pred = clf.predict(X_test)
-
-# Оцениваем точность модели
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-
-# Теперь, используя обученную модель, вы можете предсказывать состояние движения на новых данных.
-new_data = pd.DataFrame({'x': [0.1], 'y': [0.2], 'z': [0.3], 'calculated_speed': [10.0]})
-prediction = clf.predict(new_data)
-print("Prediction:", prediction)
 
 def predict_transportation(model, new_data):
-    prediction = model.predict(new_data)
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = new_data[features]
+    prediction = model.predict(X)
     transportation_modes = {
         0: "Автомобиль",
         1: "Автобус",
@@ -99,6 +41,32 @@ def predict_transportation(model, new_data):
     }
     transportation_mode = transportation_modes.get(prediction[0], "Неизвестно")
     return transportation_mode
+
+
+def main():
+    st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
+    st.title("MoveMate")
+    st.header("Halte alle auf dem Laufenden")
+    
+    uploaded_file = st.file_uploader("Datei hochladen", type="json")
+    
+    if uploaded_file is not None:
+        df = pd.read_json(uploaded_file)
+        df = preprocess_data(df)
+        model = train_model(df)
+        
+        movement_prediction = predict_movement(model, df)
+        transportation = predict_transportation(model, df)
+    
+        st.write(f"Aktivität: {bool(movement_prediction[0])}")
+        st.write(f"Tranport type: {transportation}")
+    else:
+        st.write("Laden Sie bitte Datei.json hoch!")
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 """def main():
--- codeapp.py
+++ codeapp.py
@@ -5,62 +5,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
-from telegram.ext import Updater, CommandHandler, MessageHandler
-from telegram.ext import filters
-import telegram_component
 
 
-def load_data():
-    bus_1 = pd.read_json("Bus1_Bauvereinstr.-Technische Hochschule-Dürrenhof.23-05-23_18-29-17.json")
-    bus_2 = pd.read_json("Bus2_Dürrenhof-Stephanstr.23-05-23_18-31-17.json")
-    # Add other datasets as needed
-    bus_df = pd.concat([bus_1, bus_2], ignore_index=True)
-    return bus_df
+def main():
+    st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
 
+    st.title("MoveMate")
+    st.header("Halte alle auf dem Laufenden")
+    st.write("Um die Funktionen von MoveMate zu nutzen, schauen Sie bitte die Instruktion im linken Menü an!")
+    
+    st.write("Nach dem Lesen der Instruktion und der Konfiguration von MoveMateBot in Telegram können wir jetzt loslegen!")
+    
+    uploaded_file = st.file_uploader("Datei hochladen", type="json")
+    
+    if uploaded_file is not None:
+        df = pd.read_json(uploaded_file)
+        df = preprocess_data(df)
+        model = train_model(df)
+        return model,df
+    else:
+        st.write("Laden Sie bitte Datei.json hoch!")
 
-def preprocess_data(df):
-    # Preprocess data here (remove null values, drop unnecessary columns, calculate speed, etc.)
-    df = df.dropna(subset=['time', 'seconds_elapsed'])
-    df['z'].fillna(df['z'].mean(), inplace=True)
-    df['y'].fillna(df['y'].mean(), inplace=True)
-    df['x'].fillna(df['x'].mean(), inplace=True)
+    movement_prediction = predict_movement(model, df)# Make the prediction
+    transportation = predict_transportation(model, df)
+    
 
-    initial_velocity = 0.0
-    df['seconds_elapsed'] = pd.to_numeric(df['seconds_elapsed'], errors='coerce')
-    df['z'] = pd.to_numeric(df['z'], errors='coerce')
-    df['calculated_speed'] = initial_velocity + df['seconds_elapsed'].diff() * df['z']
-    df.loc[:, 'calculated_speed'].fillna(df['calculated_speed'].mean(), inplace=True)
+    # Display the result
+    st.write(f"Aktivität: {bool(movement_prediction[0])}")
+    st.write(f"Tranport type: {transportation}")
 
-    threshold_acceleration = 0.1
-    df['moving'] = df[['x', 'y', 'z']].apply(lambda row: any(abs(row) > threshold_acceleration), axis=1)
-    df['moving_label'] = df['moving'].astype(int)
-
-    return df
-
-
-def train_model(df):
-    features = ['x', 'y', 'z', 'calculated_speed']
-    X = df[features]
-    y = df['moving_label']
-
-    # Train the model
-    clf = DecisionTreeClassifier()
-    clf.fit(X, y)
-    return clf
-
-
-def predict_movement(model, new_data):
-    prediction = model.predict(new_data)
-    return prediction
-
-
-def predict_transportation(model, new_data):
-    prediction = model.predict(new_data)
-    transportation_modes = {
-        0: "Автомобиль",
-        1: "Автобус",
-        2: "Метро",
-        3: "Велосипед"
-    }
-    transportation_mode = transportation_modes.get(prediction[0], "Неизвестно")
-    return transportation_mode
+if __name__ == "__main__":
+    
+    main()
