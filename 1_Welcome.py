57a86e1fde2083d2858e88e327caaa7b8bf7dd77
Anna Subbotina <anna.subbotina@fau.de>
2023-07-07 02:46:53 +0500

    UPD

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -1,124 +1,96 @@
+#importing necessery libraries for future analysis of the dataset
+#import folium
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+#import matplotlib.pyplot as plt
+#import matplotlib as plt
+#import seaborn as sns
+#import json
+import numpy as np
 import streamlit as st
-from PIL import Image
-from streamlit_lottie import st_lottie
-import requests
+#from streamlit_folium import st_folium
+#import plotly.express as px
+import torch
 
-@st.cache_data
+model_tree = 'Tree_Model.pkl'
+model = torch.load(model_tree)
 
-def load_data():
-    bus_1 = pd.read_json("Bus1_Bauvereinstr.-Technische Hochschule-Dürrenhof.23-05-23_18-29-17.json")
-    bus_2 = pd.read_json("Bus2_Dürrenhof-Stephanstr.23-05-23_18-31-17.json")
-    fahrrad_1 = pd.read.json("Fahrraddata2.json")
-    fahrrad_2 = pd.read.json("Fahrraddata3.json")
-    ubahn_1 = pd.read.json("uBahn_Kaulbachplatz_-_Hbf-2023-05-24_06-09-08.json")
-    ubahn_2 = pd.read.json("Ubahn_Wöhrder Wiese-Hbf-Opernhaus.24-05-23_13-33-39.json")
-    # Add other datasets as needed
-    bf_df = pd.concat([bus_1, bus_2, fahrrad_1, fahrrad_2, ubahn_1, ubahn_2], ignore_index=True)
-    return bf_df
+from collections import Counter
+ 
+# Function to get unique values
+ 
+ 
+def unique(list1):
+ 
+    # Print directly by using * symbol
+    print(*Counter(list1))
+#Functions 
 
-def preprocess_data(df):
-    # Preprocess data here (remove null values, drop unnecessary columns, calculate speed, etc.)
-    df = df.dropna(subset=['time', 'seconds_elapsed'])
-    df['z'].fillna(df['z'].mean(), inplace=True)
-    df['y'].fillna(df['y'].mean(), inplace=True)
-    df['x'].fillna(df['x'].mean(), inplace=True)
-
-    initial_velocity = 0.0
-    df['seconds_elapsed'] = pd.to_numeric(df['seconds_elapsed'], errors='coerce')
-    df['z'] = pd.to_numeric(df['z'], errors='coerce')
-    df['calculated_speed'] = initial_velocity + df['seconds_elapsed'].diff() * df['z']
-    df.loc[:, 'calculated_speed'].fillna(df['calculated_speed'].mean(), inplace=True)
-
-
-    threshold_acceleration = 0.1
-    df['moving'] = df[['x', 'y', 'z']].apply(lambda row: any(abs(row) > threshold_acceleration), axis=1)
-    df['moving_label'] = df['moving'].astype(int)
-
-    return df
-
-
-def train_model(df):
-    features = ['x', 'y', 'z', 'calculated_speed']
-    X = df[features]
-    y = df['moving_label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    clf = DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-    return clf
-
-
-def predict_movement(model, df):
-    features = ['x', 'y', 'z', 'calculated_speed']
-    X = df[features]
-    prediction = model.predict(X)
-    return prediction
-
-
-def predict_transportation(model, new_data):
-    features = ['x', 'y', 'z', 'calculated_speed']
-    X = new_data[features]
-    prediction = model.predict(X)
-    transportation_modes = {
-        0: "Auto",
-        1: "Bus",
-        2: "U-Bahn",
-        3: "Fahrrad"
-    }
-    transportation_mode = transportation_modes.get(prediction[0], "Unbekannt")
-    return transportation_mode
-
-#Streamlit App code below
-
-def main():
-    st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
-
-    def load_lottieurl(url):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
+#Tranform accelerometer and gyroscope data to one dataframe
+def transform_data_acceleration(df):   
+    acce = df[df['sensor'] == 'Accelerometer']
+    acce.reset_index(drop=True, inplace=True)   
+    acce = acce.drop(columns =['seconds_elapsed','sensor', 'relativeAltitude', 'pressure', 'altitude', 'speedAccuracy', 'bearingAccuracy', 'latitude', 'altitudeAboveMeanSeaLevel', 'bearing', 'horizontalAccuracy', 'verticalAccuracy', 'longitude', 'speed', 'version', 'device name', 'recording time', 'platform', 'appVersion', 'device id', 'sensors', 'sampleRateMs', 'yaw', 'qx', 'qz', 'roll', 'qw', 'qy', 'pitch'])
+    acce['Magnitude_acce'] = np.sqrt(acce["x"] ** 2 + acce["y"] ** 2 + acce["z"] ** 2)
     
-    logo = Image.open("logo.jpg")
+    gyro = df[df['sensor'] == 'Gyroscope']
+    gyro.reset_index(drop=True, inplace=True)   
+    gyro = gyro.drop(columns = ['seconds_elapsed','sensor', 'relativeAltitude', 'pressure', 'altitude', 'speedAccuracy', 'bearingAccuracy', 'latitude', 'altitudeAboveMeanSeaLevel', 'bearing', 'horizontalAccuracy', 'verticalAccuracy', 'longitude', 'speed', 'version', 'device name', 'recording time', 'platform', 'appVersion', 'device id', 'sensors', 'sampleRateMs', 'yaw', 'qx', 'qz', 'roll', 'qw', 'qy', 'pitch'])
 
-    st.sidebar.success("Menu")
+    for df in [gyro, acce]:
+         df.index = pd.to_datetime(df['time'], unit = 'ns',errors='ignore')
+         df.drop(columns=['time'], inplace=True)
+    #df_new = pd.merge(loc, gyro, suffixes=('_loc', '_gyro'), on='time')
+    df_new = acce.join(gyro, lsuffix = '_acce', rsuffix = '_gyro', how = 'outer').interpolate()
+    
+    min_values = df_new.resample('15s').min(numeric_only=True)
+    max_values = df_new.resample('15s').max(numeric_only=True)
+    mean_values = df_new.resample('15s').mean(numeric_only=True)
+    std_values = df_new.resample('15s').std(numeric_only=True)
 
-    with st.container():
-        text_column, image_column = st.columns((2,1))
-        with text_column:
-            st.title("MoveMate")
-            st.header("Keep everyone on track")
-            st.markdown("[See our GitHub Repository -->] (https://github.com/or81ynez/MaennerML)")
-        with image_column:
-            st.image(logo)
+    columns_to_drop = df_new.columns.difference(['Magnitude_acce', 'x_gyro', 'y_gyro', 'z_gyro'])
+    for df in [min_values, max_values, mean_values, std_values]:
+        df.drop(columns=columns_to_drop, inplace=True)
+    feature_df = pd.merge(pd.merge(min_values, max_values, suffixes = ('_min', '_max'), on = 'time'), pd.merge(mean_values, std_values, suffixes = ('_mean', '_std'), on = 'time'), on = 'time')
+    prediction= model.predict(feature_df)
+    result = unique(prediction)
+    return result
 
-    with st.container():
-        st.write("---")
-        st.write("With our MoveMateApp you will be able to determine the type of transport on which you move.")
-        st.write("But in order to get started we advise you to read the Instructions section on the left in the menu!")
-    with st.container():
-        st.write("---")
-        left_column, right_column = st.columns(2)
-        lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_xbf1be8x.json")
-        with left_column:
-            st.write("We hope you managed to record your movement data. Let's try to determine the type of your transport!")
-            uploaded_file = st.file_uploader("Datei hochladen", type="json")
-            if uploaded_file is not None:
-                user_df = pd.read_json(uploaded_file)
-                user_df = preprocess_data(user_df) 
-                model = train_model(user_df)
-                transportation = predict_transportation(model, user_df)
-                st.write(f"Tranport type: {transportation}")
-            else:
-                st.write("Laden Sie bitte Datei.json hoch!")
-        with right_column:
-                st_lottie(lottie_coding, height=300, key="coding")
-        
 
-if __name__ == "__main__":
-    main()
+#Tranform location from file
+def transform_data_location(df):
+    location = df[df['sensor'] == 'Location']
+    location.reset_index(drop=True, inplace=True)
+    location = location.drop(columns = ['sensor', 'z', 'y', 'x', 'relativeAltitude', 'pressure', 'version', 
+                                        'device name', 'recording time', 'platform', 'appVersion', 'device id', 'sensors', 'sampleRateMs', 'yaw', 'qx', 'qz', 'roll', 'qw', 'qy', 'pitch'])
+    #Speed using abs to positive
+    
+    location.index = pd.to_datetime(location['time'], unit = 'ns',errors='ignore')
+    location.drop(columns=['time'], inplace=True)
+    #location['Type'] = type
+    return location
+ 
+### Streamlit Area
+### Page config
+st.set_page_config(page_title="Mobility Classification App", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
+
+st.header("Dein Standort wurde gefunden")
+with st.container():
+    st.write("---")
+    #st.subheader("Du bist gerade auf...")
+    st.write("Fügen Sie bitte ihre Datei hinzu")
+    def main():
+        file = st.file_uploader("Laden Sie eine JSON Datei hoch!", type = ['json'])
+        if file is not None:
+            uploaded_file = pd.read_json(file)
+            if st.button("Classify me!"):
+                prediction =  transform_data_acceleration(uploaded_file)
+                location_data = transform_data_location(uploaded_file)
+
+                st.subheader("Dein Fortbewegungsgraph")
+                st.map(location_data)
+            
+                st.caption("Du bist gerade auf dem " + str(prediction) + "!")
+                
+    if __name__ == "__main__":
+        main()
+    st.write("---")
---
+++ codecode.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import streamlit as st
+from PIL import Image
+from streamlit_lottie import st_lottie
+import requests
+
+@st.cache_data
+
+def load_data():
+    bus_1 = pd.read_json("Bus1_Bauvereinstr.-Technische Hochschule-Dürrenhof.23-05-23_18-29-17.json")
+    bus_2 = pd.read_json("Bus2_Dürrenhof-Stephanstr.23-05-23_18-31-17.json")
+    fahrrad_1 = pd.read.json("Fahrraddata2.json")
+    fahrrad_2 = pd.read.json("Fahrraddata3.json")
+    ubahn_1 = pd.read.json("uBahn_Kaulbachplatz_-_Hbf-2023-05-24_06-09-08.json")
+    ubahn_2 = pd.read.json("Ubahn_Wöhrder Wiese-Hbf-Opernhaus.24-05-23_13-33-39.json")
+    # Add other datasets as needed
+    bf_df = pd.concat([bus_1, bus_2, fahrrad_1, fahrrad_2, ubahn_1, ubahn_2], ignore_index=True)
+    return bf_df
+
+def preprocess_data(df):
+    # Preprocess data here (remove null values, drop unnecessary columns, calculate speed, etc.)
+    df = df.dropna(subset=['time', 'seconds_elapsed'])
+    df['z'].fillna(df['z'].mean(), inplace=True)
+    df['y'].fillna(df['y'].mean(), inplace=True)
+    df['x'].fillna(df['x'].mean(), inplace=True)
+
+    initial_velocity = 0.0
+    df['seconds_elapsed'] = pd.to_numeric(df['seconds_elapsed'], errors='coerce')
+    df['z'] = pd.to_numeric(df['z'], errors='coerce')
+    df['calculated_speed'] = initial_velocity + df['seconds_elapsed'].diff() * df['z']
+    df.loc[:, 'calculated_speed'].fillna(df['calculated_speed'].mean(), inplace=True)
+
+
+    threshold_acceleration = 0.1
+    df['moving'] = df[['x', 'y', 'z']].apply(lambda row: any(abs(row) > threshold_acceleration), axis=1)
+    df['moving_label'] = df['moving'].astype(int)
+
+    return df
+
+
+def train_model(df):
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = df[features]
+    y = df['moving_label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+    return clf
+
+
+def predict_movement(model, df):
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = df[features]
+    prediction = model.predict(X)
+    return prediction
+
+
+def predict_transportation(model, new_data):
+    features = ['x', 'y', 'z', 'calculated_speed']
+    X = new_data[features]
+    prediction = model.predict(X)
+    transportation_modes = {
+        0: "Auto",
+        1: "Bus",
+        2: "U-Bahn",
+        3: "Fahrrad"
+    }
+    transportation_mode = transportation_modes.get(prediction[0], "Unbekannt")
+    return transportation_mode
+
+#Streamlit App code below
+
+def main():
+    st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
+
+    def load_lottieurl(url):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+    
+    logo = Image.open("logo.jpg")
+
+    st.sidebar.success("Menu")
+
+    with st.container():
+        text_column, image_column = st.columns((2,1))
+        with text_column:
+            st.title("MoveMate")
+            st.header("Keep everyone on track")
+            st.markdown("[See our GitHub Repository -->] (https://github.com/or81ynez/MaennerML)")
+        with image_column:
+            st.image(logo)
+
+    with st.container():
+        st.write("---")
+        st.write("With our MoveMateApp you will be able to determine the type of transport on which you move.")
+        st.write("But in order to get started we advise you to read the Instructions section on the left in the menu!")
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_xbf1be8x.json")
+        with left_column:
+            st.write("We hope you managed to record your movement data. Let's try to determine the type of your transport!")
+            uploaded_file = st.file_uploader("Datei hochladen", type="json")
+            if uploaded_file is not None:
+                user_df = pd.read_json(uploaded_file)
+                user_df = preprocess_data(user_df) 
+                model = train_model(user_df)
+                transportation = predict_transportation(model, user_df)
+                st.write(f"Tranport type: {transportation}")
+            else:
+                st.write("Laden Sie bitte Datei.json hoch!")
+        with right_column:
+                st_lottie(lottie_coding, height=300, key="coding")
+        
+
+if __name__ == "__main__":
+    main()
