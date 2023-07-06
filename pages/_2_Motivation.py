4bb27ea2617184184444f227482bec439bc488ed
Anna Subbotina <anna.subbotina@fau.de>
2023-07-06 15:08:09 +0500

    upd

--- pages/_2_Motivation.py
+++ pages/_2_Motivation.py
@@ -16,7 +16,7 @@ import json
 import streamlit as st
 from streamlit_lottie import st_lottie
 
-st.set_page_config(page_title="Mobility Classification App", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="MoveMate", page_icon=":oncoming_automobile:", layout="wide", initial_sidebar_state="collapsed")
 
 # Weitere Codezeilen...
 
