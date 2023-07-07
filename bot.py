951a685496b3afd518ae490d8211cbb8cb3b3af3
Anna Subbotina <anna.subbotina@fau.de>
2023-07-07 14:12:00 +0500

    upd

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 bot = telebot.TeleBot('6164194815:AAER-xuJ0bGMDlRnRcsvYLPXx-d8smdZJrQ')
 
 markup = types.InlineKeyboardMarkup()
-markup.add(types.InlineKeyboardButton('MoveMateApp', url='https://ml4bmaennerroadfocusapp-i9svonu37js.streamlit.app/'))
+markup.add(types.InlineKeyboardButton('MoveMateApp', url='https://ml4bmaennerroadfocusapp-i9svonu37js.streamlit.app/ , https://github.com/annasubbotina/ML4B_MAENNER_ROADFOCUS_APP'))
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Hi! Please press the button below to open MoveMateApp!', reply_markup=markup)
